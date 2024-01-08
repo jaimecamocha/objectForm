1a05f9734e966c632e14c6af286d09b3483b6f12
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2024-01-08 08:36:04 +0100

    Object Form

---
+++ README.md
@@ -0,0 +1,8 @@
+# Comienzo con el objeto form de JavaScript
+
+**Autor** : Jaime Fernández Calvo 
+
+| Tema                                     | Descripción                                                               |
+| ---------------------------------------- | ------------------------------------------------------------------------- |
+| **Manejo de formularios con JavaScript** | Interactividad entre una página web y el usuario a través de formularios. |
+| **Objeto `form` en JavaScript**          | Propiedades únicas, como `action` para indicar la acción del formulario.  |
---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+/**
+ * @author Jaime Fernández Calvo
+ * @github https://github.com/DiegusNueva/objectForm
+ */
+
+// Método 1: acceder a los formularios
+
+const formulario = document.getElementById('contactar')
+
+// Método 2: acceder a los formularios
+
+const formularios = document.getElementsByTagName('form');
+const primerFormulario = formularios[0];	// primer formulario del documento
+
+// Método 3: acceder a los formularios
+
+// la referencia a todos los formularios del documento
+const misFormularios = document.forms;
+
+// primer formulario del documento
+const miPrimerFormulario1 = misFormularios[0];
+
+// o bien primer formulario del documento
+const miPrimerFormulario2 = document.forms[0];
+
+// o bien referenciamos al formulario con name "contactar"
+const miPrimerFormulario3 = miPrimerFormulario2["contactar"];
+
